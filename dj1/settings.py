841fff8912030beda36d0d73b1bb060b0a52cfab
ShaliniR8 <61011528+ShaliniR8@users.noreply.github.com>
2020-06-27 00:45:16 +0530

    Update settings.py

--- dj1/settings.py
+++ dj1/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ggiydivil&s7*oonav=_i$k81n7!e2$zffe*t#5u$hbwx8&%ii'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
